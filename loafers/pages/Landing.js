b9965cd09db24b1dcd441e92868c2b9f2b03e544
Jack C <jacktheclark@gmail.com>
2024-05-03 09:41:02 -0700

    changes

--- loafers/pages/Landing.js
+++ loafers/pages/Landing.js
@@ -23,7 +23,8 @@ export default function Landing({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.instructionsText}>bleh bleh bleh</Text>
+      <Text style={styles.instructionsText}>Welcome to TownHall</Text>
+      <Text style={styles.subtitleText}>Please choose a pseudonym to continue</Text>
       <View style={styles.row}>
         <TouchableOpacity style={styles.generateButton} onPress={generateName}>
           <Ionicons name="dice" size={32} color={COLORS.lightaccent} />
@@ -33,7 +34,7 @@ export default function Landing({ navigation }) {
         </View>
       </View>
       <TouchableOpacity style={styles.loginButton} onPress={handlePressLogin}>
-        <Text style={styles.loginButtonText}>Go to feed</Text>
+        <Text style={styles.loginButtonText}>Continue to Discussion</Text>
       </TouchableOpacity>
     </View>
   );
@@ -47,10 +48,21 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.dark,
   },
   instructionsText: {
+    alignItems: 'center',
+    justifyContent: 'center',
     fontSize: 20,
     fontFamily: FONTS.bold,
     color: COLORS.lightaccent,
     marginTop: 60, // Move the instructions towards the top
+  },
+  subtitleText: {
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 16,
+    fontFamily: FONTS.medium,
+    color: COLORS.lightaccent,
+    marginTop: 20, // Move the instructions towards the top
   },
   row: {
     flexDirection: 'row',
@@ -58,19 +70,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   nameContainer: {
-    width: 300, // Set a fixed width for the name container
+    width: 300,
+    height: 40,
     margin: 10,
     borderWidth: 1,
     borderColor: 'gray',
     padding: 5,
     borderRadius: 5,
-    justifyContent: 'center', // Center the content vertically
+    justifyContent: 'center',
   },
   nameText: {
     fontSize: 24,
     fontFamily: FONTS.body,
     color: COLORS.lightaccent,
-    textAlign: 'center', // Center the name
+    textAlign: 'center',
   },
   loginButton: {
     backgroundColor: COLORS.lightaccent,
