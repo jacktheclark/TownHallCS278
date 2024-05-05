ccc8b61ab9ef823394f6c65a125bb60d681a1e3f
Yishu Chen <ychen23@stanford.edu>
2024-05-05 14:00:48 -0700

    Some style things, header color & text

--- loafers/pages/Feed.js
+++ loafers/pages/Feed.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import {COLORS, FONTS} from "../constants.js";
-import CommentsFeed from '../components/CommentsFeed.js';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { COLORS, FONTS } from "../constants.js";
+import CommentsFeed from "../components/CommentsFeed.js";
 
 export default function Feed() {
   return (
     <View style={styles.container}>
-      <Text style={styles.testText}>feed screen</Text>
-      <CommentsFeed/>
-      
+      {/* <Text style={styles.testText}>feed screen</Text> */}
+      <CommentsFeed />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: COLORS.dark,
-      },
-    testText: {
-        marginTop: 100,
-        marginBottom: 50,
-        color: 'white',
-        fontSize: 24,
-        fontFamily: FONTS.bold,
-        color: COLORS.lightaccent,
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.dark,
+  },
+  testText: {
+    marginTop: 100,
+    marginBottom: 50,
+    color: "white",
+    fontSize: 24,
+    fontFamily: FONTS.bold,
+    color: COLORS.lightaccent,
+  },
 });
